3199c90fcb420cd27deac9fe6dfbbad6407b83eb
JayIssuree <jay_issuree@hotmail.co.uk>
2021-01-08 14:00:36 +0000

    docking stations release working bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,6 +8,14 @@ describe DockingStation do
             expect(subject).to respond_to(:release_bike)
         end
 
+        it 'should release a bike object' do
+            expect(subject.release_bike).to be_an_instance_of(Bike)
+        end
+
+        it 'should release working bike' do
+            expect(subject.release_bike.working?).to be(true)
+        end
+
     end
     
 end
