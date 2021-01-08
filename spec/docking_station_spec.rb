507e4f236f249d599a85c979f2bfdd10d576f2db
JayIssuree <jay_issuree@hotmail.co.uk>
2021-01-08 15:18:28 +0000

    stations do not release_bike if none are available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,11 +8,19 @@ describe DockingStation do
             expect(subject).to respond_to(:release_bike)
         end
 
+        it 'should raise an error if there are no bikes to release' do
+            expect { subject.release_bike }.to raise_error("No bikes available")
+        end
+
         it 'should release a bike object' do
-            expect(subject.release_bike).to be_an_instance_of(Bike)
+            bike = Bike.new
+            subject.dock(bike)
+            expect(subject.release_bike).to eq(bike)
         end
 
         it 'should release working bike' do
+            bike = Bike.new
+            subject.dock(bike)
             expect(subject.release_bike.working?).to be(true)
         end
 
