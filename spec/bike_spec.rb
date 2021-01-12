543cda93a303b9ac105ce04cab3d03313b5ada26
JayIssuree <jay_issuree@hotmail.co.uk>
2021-01-12 15:11:57 +0000

    bikes can be reported as broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -8,6 +8,19 @@ describe Bike do
             expect(subject).to respond_to(:working?)
         end
 
+        it 'should be working by default' do
+            expect(subject).to be_working
+        end
+
+    end
+
+    describe '#report_broken' do
+        
+        it 'should not be working when reported broken' do
+            subject.report_broken
+            expect(subject).not_to be_working
+        end
+
     end
 
 end
