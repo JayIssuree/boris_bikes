c3a2338406b60b524332b53832d3d566dde01f8b
JayIssuree <jay_issuree@hotmail.co.uk>
2021-01-21 17:55:41 +0000

    removed magic numbers from van tests

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -8,24 +8,11 @@ describe Van do
         :bikes => { working: [working_bike, working_bike], broken:[broken_bike, broken_bike] }
     }
     let(:garage) { double :garage,
-        :bikes => { working: [working_bike, working_bike], broken:[] }
+        :bikes => { working: [working_bike, working_bike], broken:[broken_bike, broken_bike] }
     }
-    
-    describe '#pick_up_broken_bikes(station)' do
+
+    describe '#initialize' do
         
-        it 'should pick up broken bikes from a station' do
-            subject.pick_up_broken_bikes(station)
-            expect(subject.bikes[:broken]).to include(broken_bike)
-            expect(subject.bikes[:broken].length).to eq(2)
-            expect(subject.bikes[:working]).to be_empty
-        end
-
-        it 'should not pick up a broken bike when at defualt capacity' do
-            Van::DEFAULT_CAPACITY.times { station.bikes[:broken] << broken_bike }
-            expect{ subject.pick_up_broken_bikes(station) }.to raise_error("Van at capacity")
-            expect(subject.bikes[:broken].length).to eq(Van::DEFAULT_CAPACITY)
-        end
-
         it 'should initialize a van with a capacity of choice' do
             new_capacity = 5
             subject = Van.new(new_capacity)
@@ -35,15 +22,37 @@ describe Van do
         end
 
     end
+    
+    describe '#pick_up_broken_bikes(station)' do
+        
+        it 'should pick up broken bikes from a station' do
+            expect{ subject.pick_up_broken_bikes(station) }
+                .to change { subject.bikes[:broken].length }.by(2)
+                .and change { station.bikes[:broken].length}.by(-2)
+            expect(station.bikes[:broken]).to be_empty
+            expect(subject.bikes[:working]).to be_empty
+        end
+
+        it 'should not pick up a broken bike when at defualt capacity' do
+            Van::DEFAULT_CAPACITY.times { station.bikes[:broken] << broken_bike }
+            expect{ subject.pick_up_broken_bikes(station) }.to raise_error("Van at capacity")
+            expect(subject.bikes[:broken].length).to eq(Van::DEFAULT_CAPACITY)
+        end
+
+    end
 
     describe '#drop_off_broken_bikes(garage)' do
         
         it 'should drop off broken bikes to a garage' do
             subject.pick_up_broken_bikes(station)
-            expect(subject.bikes[:broken].length).to eq(2)
-            subject.drop_off_broken_bikes(garage)
+            expect{ subject.drop_off_broken_bikes(garage) }
+                .to change { subject.bikes[:broken].length }.by(-2)
+                .and change { garage.bikes[:broken].length }.by(2)
             expect(subject.bikes[:broken]).to be_empty
-            expect(garage.bikes[:broken].length).to eq(2)
+        end
+
+        it 'should not drop off broken bikes when the garage is at capacity' do
+            
         end
 
     end
@@ -51,10 +60,17 @@ describe Van do
     describe '#pick_up_working_bikes(garage)' do
         
         it 'should pick up working bikes from a garage' do
-            subject.pick_up_working_bikes(garage)
-            expect(subject.bikes[:working]).to include(working_bike)
-            expect(subject.bikes[:working].length).to eq(2)
+            expect{ subject.pick_up_working_bikes(garage) }
+                .to change { subject.bikes[:working].length }.by(2)
+                .and change { garage.bikes[:working].length}.by(-2)
+            expect(garage.bikes[:working]).to be_empty
             expect(subject.bikes[:broken]).to be_empty
+        end
+
+        it 'should not pick up a working bike when at defualt capacity' do
+            Van::DEFAULT_CAPACITY.times { garage.bikes[:working] << working_bike }
+            expect{ subject.pick_up_working_bikes(garage) }.to raise_error("Van at capacity")
+            expect(subject.bikes[:working].length).to eq(Van::DEFAULT_CAPACITY)
         end
 
     end
@@ -63,10 +79,10 @@ describe Van do
         
         it 'should drop off working bikes to a station' do
             subject.pick_up_working_bikes(garage)
-            expect(subject.bikes[:working].length).to eq(2)
-            subject.drop_off_working_bikes(station)
+            expect{ subject.drop_off_working_bikes(station) }
+                .to change { subject.bikes[:working].length }.by(-2)
+                .and change { station.bikes[:working].length}.by(2)
             expect(subject.bikes[:working]).to be_empty
-            expect(station.bikes[:working].length).to eq(4)
         end
 
     end
